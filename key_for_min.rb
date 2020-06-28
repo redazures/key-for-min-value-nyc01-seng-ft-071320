2c37859e3e5452903ccae1b58f6b35af0e86590b
james wu <jwu_nyc@yahoo.com>
2020-06-28 19:06:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowest=""
   tracker=100000000
   name_hash.each do |key,value|
-    puts key
+    lowest=key if value<tracker
     # if value<tracker
   end
   # lowest
