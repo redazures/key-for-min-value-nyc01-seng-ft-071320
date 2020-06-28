e47e87363cbe894cdf9ca29e9180773b0164ddc4
james wu <jwu_nyc@yahoo.com>
2020-06-28 19:05:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,10 @@ def key_for_min_value(name_hash)
   lowest=""
   tracker=100000000
   name_hash.each do |key,value|
-    puts key if value<tracker
+    puts key
+    # if value<tracker
   end
-  name_hash[lowest]
+  # lowest
 end
 
 ikea = {:chair => 25, :table => 85, :mattress => 450}
